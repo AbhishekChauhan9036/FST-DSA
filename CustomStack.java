5fe28838005f22dc3d98e4207545fe10002856e2
Abhishek Chauhan <abhishekchauhan9036@gmail.com>
2024-01-08 15:47:45 +0530

    msg

--- CustomStack.java
+++ CustomStack.java
@@ -1,53 +1,50 @@
-import java.util.Stack;
-
-public class CustomStack {
-    private Stack<Integer> items;
-
-    public CustomStack() {
-        this.items = new Stack<>();
+import java.util.*;
+public class CustomStack{
+    private Stack<Integer>items;
+    public CustomStack(){
+        this.items=new Stack<>();
     }
 
-    public void push(int element) {
+    public boolean isEmpty(){
+        return items.isEmpty();
+    }
+
+    public void push(int element){
         items.push(element);
     }
 
-    public int pop() {
-        if (isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
+    public int pop(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
         }
         return items.pop();
     }
 
-    public int peek() {
-        if (isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
+
+    public int peek(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
         }
         return items.peek();
     }
 
-    public boolean isEmpty() {
-        return items.isEmpty();
-    }
-
-    public int size() {
+    public int size(){
         return items.size();
     }
 
-    public void print() {
+    public void print(){
         System.out.println(items.toString());
     }
 
-    public static void main(String[] args) {
-        CustomStack stack = new CustomStack();
-
-        System.out.println(stack.isEmpty());
-        stack.push(20);
-        stack.push(10);
-        stack.push(30);
-        System.out.println(stack.size());
-        stack.print();
-        System.out.println(stack.pop());
-        System.out.println(stack.peek());
-        stack.print();
+    public static void main(String... args){
+            CustomStack stack=new CustomStack();
+            // System.out.println(stack.isEmpty());
+            stack.push(10);
+            stack.push(20);
+            // stack.print();
+            // stack.pop();
+            System.out.println(stack.peek());
+             System.out.println(stack.size());
+            stack.print();
     }
 }
