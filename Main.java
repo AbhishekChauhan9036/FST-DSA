5f32bc88b673188ce5cf7ea750d9088928c75a73
Abhishek Chauhan <abhishekchauhan9036@gmail.com>
2024-01-18 17:11:43 +0530

    obj

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
+interface Colorable {
+    void setColor(String color);
+    String getColor();
+}
+
+class Pen implements Colorable {
+    private String color;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        String str1 = "Java";
-        String str2 = new String("Java");
-        System.out.println(str1 == str2);
+        Pen redPen = new Pen();
+        redPen.setColor("Red");
+        System.out.println(redPen.getColor());
     }
 }
