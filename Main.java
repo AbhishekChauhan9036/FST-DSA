ac9acf451ebb29271ec9d1239581ab840b2c9a6b
Abhishek Chauhan <abhishekchauhan9036@gmail.com>
2024-02-21 12:12:52 +0530

    Bitwise Operator

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-class Main {
-    public static void main(String[] args) {
-        if (1 + 1 + 1 + 1 + 1 == 5) {
-            System.out.print("TRUE");
-        } else {
-            System.out.print("FALSE");
+public class Main {
+
+    public static int rangeBitwiseAnd(int left, int right) {
+        int ans=0;
+        while(left!=right){
+            left=left/2;
+            right=right/2;
+            ans++;
         }
+        return left<<ans;
+    }
+    public static void main(String[] args) {
+        int a=9;
+        int b=15;
+        System.out.println(rangeBitwiseAnd(a, b));
     }
 }
